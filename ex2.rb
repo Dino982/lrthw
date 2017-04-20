0052ee7cc44c818f159fd04c2cdd585a4d9efa12
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-20 21:23:58 +0400

    Ex2

---
+++ ex2.rb
@@ -0,0 +1,5 @@
+# Anything after the # is ignored
+puts "I could have code like this." # and the comment after is ignored
+# You can also use a comment to disbale or comment out a piece of code
+# puts "This won't run."
+puts "This will run"
