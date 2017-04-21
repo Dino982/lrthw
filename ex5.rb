cff94edd60930e5ef51dd0c5eae39300e3416693
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-21 14:02:49 +0400

    Ex5

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+name = 'Andrew Clarke'
+age = 35
+height = 170 #cm
+weight = 180 #lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+puts "Lets talk about my #{name}."
+puts "He is #{height} cm tall."
+puts "He is #{weight} pounds heavy"
+puts "He has got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
