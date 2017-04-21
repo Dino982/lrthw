e2a51fd27a7ac536fda4fc98048e5facf4ee84e0
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-21 14:44:56 +0400

    Ex9

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There is something going on here.
+With these weird quote chars.
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
+
+
+# \n puts in a line break for a new line
